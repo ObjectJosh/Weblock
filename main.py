0d62338a872b9cad7716b92e269afd673a641c6f
Aiyana Arnobit <anzarnobit28@gmail.com>
2021-01-24 07:50:40 -0800

    fixed main

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
 from site_blocker import *
-from adding_current_time import *
-
-def make_website_list(load):
-    websites = load[:-1]
-    website_list = []
-    for website in websites:
-        website_list.append(website[0])
-    return website_list
-
-def get_hour(load):
-    return load[-1][0]
-
-def get_minute(load):
-    return load[-1][1]
 
 if __name__ == "__main__":
     import sys
@@ -23,12 +9,8 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     ui.connectActions()
     MainWindow.show()
-    if ui.saveNum == 1:
-        load = readFile("siteNames.csv")
-        website_list = make_website_list(load)
-        hour = get_hour(load)
-        minute = get_minute(load)
-        websiteBlocker(r"C:\Windows\System32\drivers\etc", website_list, hour, minute)
     sys.exit(app.exec_())
+
+
 
 
