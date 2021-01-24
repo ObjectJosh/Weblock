41d18e08f5a9b5df6cfc634005c8afd8d3b1619f
ObjectJosh <joshwong74@gmail.com>
2021-01-23 17:28:51 -0800

    able to add new sites, scroll area, updated UI

--- site_blocker.py
+++ site_blocker.py
@@ -12,80 +12,90 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class Ui_MainWindow(object):
+    def __init__(self):
+        self.row = 6
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(787, 622)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(130, 120, 531, 124))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.sitesGridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
-        self.sitesGridLayout.setContentsMargins(0, 0, 0, 0)
-        self.sitesGridLayout.setObjectName("sitesGridLayout")
-        self.blockedSitesLabel = QtWidgets.QLabel(self.gridLayoutWidget)
-        self.blockedSitesLabel.setObjectName("blockedSitesLabel")
-        self.sitesGridLayout.addWidget(self.blockedSitesLabel, 1, 0, 1, 1)
-        self.blockedLabel = QtWidgets.QLabel(self.gridLayoutWidget)
-        self.blockedLabel.setObjectName("blockedLabel")
-        self.sitesGridLayout.addWidget(self.blockedLabel, 1, 1, 1, 1)
-        self.deleteButton2 = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.deleteButton2.setObjectName("deleteButton2")
-        self.sitesGridLayout.addWidget(self.deleteButton2, 3, 2, 1, 1, QtCore.Qt.AlignHCenter)
-        self.deleteButton1 = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.deleteButton1.setObjectName("deleteButton1")
-        self.sitesGridLayout.addWidget(self.deleteButton1, 2, 2, 1, 1, QtCore.Qt.AlignHCenter)
-        self.addNewButton = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.addNewButton.setObjectName("addNewButton")
-        self.sitesGridLayout.addWidget(self.addNewButton, 1, 2, 1, 1, QtCore.Qt.AlignHCenter)
-        self.checkBox1 = QtWidgets.QCheckBox(self.gridLayoutWidget)
-        self.checkBox1.setText("")
-        self.checkBox1.setObjectName("checkBox1")
-        self.sitesGridLayout.addWidget(self.checkBox1, 2, 1, 1, 1, QtCore.Qt.AlignHCenter)
-        self.checkBox2 = QtWidgets.QCheckBox(self.gridLayoutWidget)
-        self.checkBox2.setText("")
-        self.checkBox2.setObjectName("checkBox2")
-        self.sitesGridLayout.addWidget(self.checkBox2, 3, 1, 1, 1, QtCore.Qt.AlignHCenter)
-        self.site1 = QtWidgets.QTextEdit(self.gridLayoutWidget)
-        self.site1.setMaximumSize(QtCore.QSize(16777215, 31))
-        self.site1.setObjectName("site1")
-        self.sitesGridLayout.addWidget(self.site1, 2, 0, 1, 1)
-        self.site2 = QtWidgets.QTextEdit(self.gridLayoutWidget)
-        self.site2.setMaximumSize(QtCore.QSize(16777215, 31))
-        self.site2.setObjectName("site2")
-        self.sitesGridLayout.addWidget(self.site2, 3, 0, 1, 1)
         self.setDurationLabel = QtWidgets.QLabel(self.centralwidget)
-        self.setDurationLabel.setGeometry(QtCore.QRect(310, 279, 141, 31))
+        self.setDurationLabel.setGeometry(QtCore.QRect(300, 329, 141, 31))
         self.setDurationLabel.setObjectName("setDurationLabel")
-        self.gridLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
-        self.gridLayoutWidget_2.setGeometry(QtCore.QRect(250, 309, 266, 63))
-        self.gridLayoutWidget_2.setObjectName("gridLayoutWidget_2")
-        self.durationGridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget_2)
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(240, 359, 266, 63))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.durationGridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
         self.durationGridLayout.setContentsMargins(0, 0, 0, 0)
         self.durationGridLayout.setObjectName("durationGridLayout")
-        self.minutesLabel = QtWidgets.QLabel(self.gridLayoutWidget_2)
-        self.minutesLabel.setObjectName("minutesLabel")
-        self.durationGridLayout.addWidget(self.minutesLabel, 0, 1, 1, 1, QtCore.Qt.AlignHCenter)
-        self.hoursLabel = QtWidgets.QLabel(self.gridLayoutWidget_2)
-        self.hoursLabel.setObjectName("hoursLabel")
-        self.durationGridLayout.addWidget(self.hoursLabel, 0, 0, 1, 1, QtCore.Qt.AlignHCenter)
-        self.hoursField = QtWidgets.QTextEdit(self.gridLayoutWidget_2)
-        self.hoursField.setMaximumSize(QtCore.QSize(16777215, 31))
-        self.hoursField.setObjectName("hoursField")
-        self.durationGridLayout.addWidget(self.hoursField, 1, 0, 1, 1)
-        self.minutesField = QtWidgets.QTextEdit(self.gridLayoutWidget_2)
+        self.minutesField = QtWidgets.QTextEdit(self.gridLayoutWidget)
         self.minutesField.setMaximumSize(QtCore.QSize(16777215, 31))
         self.minutesField.setObjectName("minutesField")
         self.durationGridLayout.addWidget(self.minutesField, 1, 1, 1, 1)
+        self.minutesLabel = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.minutesLabel.setObjectName("minutesLabel")
+        self.durationGridLayout.addWidget(self.minutesLabel, 0, 1, 1, 1, QtCore.Qt.AlignHCenter)
+        self.hoursField = QtWidgets.QTextEdit(self.gridLayoutWidget)
+        self.hoursField.setMaximumSize(QtCore.QSize(16777215, 31))
+        self.hoursField.setObjectName("hoursField")
+        self.durationGridLayout.addWidget(self.hoursField, 1, 0, 1, 1)
+        self.hoursLabel = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.hoursLabel.setObjectName("hoursLabel")
+        self.durationGridLayout.addWidget(self.hoursLabel, 0, 0, 1, 1, QtCore.Qt.AlignHCenter)
         self.titleLabel = QtWidgets.QLabel(self.centralwidget)
         self.titleLabel.setGeometry(QtCore.QRect(270, 40, 231, 51))
         self.titleLabel.setObjectName("titleLabel")
         self.saveButton = QtWidgets.QPushButton(self.centralwidget)
-        self.saveButton.setGeometry(QtCore.QRect(320, 410, 111, 31))
+        self.saveButton.setGeometry(QtCore.QRect(320, 450, 111, 31))
         self.saveButton.setObjectName("saveButton")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setGeometry(QtCore.QRect(100, 100, 576, 200)) # size for scroll area
+        self.scrollArea.setMaximumSize(QtCore.QSize(576, 16777215))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 559, 136))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.gridLayout = QtWidgets.QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout.setObjectName("gridLayout")
+        self.site1 = QtWidgets.QTextEdit(self.scrollAreaWidgetContents)
+        self.site1.setMinimumSize(QtCore.QSize(351, 31))
+        self.site1.setMaximumSize(QtCore.QSize(351, 31))
+        self.site1.setObjectName("site1")
+        self.gridLayout.addWidget(self.site1, 4, 0, 1, 1)
+        self.deleteButton2 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.deleteButton2.setObjectName("deleteButton2")
+        self.gridLayout.addWidget(self.deleteButton2, 5, 2, 1, 1)
+        self.checkBox1 = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
+        self.checkBox1.setText("")
+        self.checkBox1.setObjectName("checkBox1")
+        self.gridLayout.addWidget(self.checkBox1, 4, 1, 1, 1, QtCore.Qt.AlignHCenter)
+        self.deleteButton1 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.deleteButton1.setObjectName("deleteButton1")
+        self.gridLayout.addWidget(self.deleteButton1, 4, 2, 1, 1)
+        self.checkBox2 = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
+        self.checkBox2.setText("")
+        self.checkBox2.setObjectName("checkBox2")
+        self.gridLayout.addWidget(self.checkBox2, 5, 1, 1, 1, QtCore.Qt.AlignHCenter)
+        self.blockedLabel = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.blockedLabel.setObjectName("blockedLabel")
+        self.gridLayout.addWidget(self.blockedLabel, 2, 1, 1, 1)
+        self.addNewButton = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.addNewButton.setObjectName("addNewButton")
+        self.gridLayout.addWidget(self.addNewButton, 2, 2, 1, 1)
+        self.blockedSitesLabel = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.blockedSitesLabel.setObjectName("blockedSitesLabel")
+        self.gridLayout.addWidget(self.blockedSitesLabel, 2, 0, 1, 1)
+        self.site2 = QtWidgets.QTextEdit(self.scrollAreaWidgetContents)
+        self.site2.setMinimumSize(QtCore.QSize(351, 31))
+        self.site2.setMaximumSize(QtCore.QSize(351, 31))
+        self.site2.setObjectName("site2")
+        self.gridLayout.addWidget(self.site2, 5, 0, 1, 1)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 787, 22))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -98,30 +108,31 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.blockedSitesLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Blocked Sites</span></p></body></html>"))
-        self.blockedLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Blocked</span></p></body></html>"))
-        self.deleteButton2.setText(_translate("MainWindow", "Delete"))
-        self.deleteButton1.setText(_translate("MainWindow", "Delete"))
-        self.addNewButton.setText(_translate("MainWindow", "Add New"))
         self.setDurationLabel.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:24pt;\">Set Duration</span></p></body></html>"))
-        self.minutesLabel.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:18pt;\">Minutes</span></p></body></html>"))
-        self.hoursLabel.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:18pt;\">Hours</span></p></body></html>"))
-        self.hoursField.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
         self.minutesField.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.minutesLabel.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:18pt;\">Minutes</span></p></body></html>"))
+        self.hoursField.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.hoursLabel.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:18pt;\">Hours</span></p></body></html>"))
         self.titleLabel.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:36pt;\">Web Blocker</span></p></body></html>"))
         self.saveButton.setText(_translate("MainWindow", "Save"))
+        self.deleteButton2.setText(_translate("MainWindow", "Delete"))
+        self.deleteButton1.setText(_translate("MainWindow", "Delete"))
+        self.blockedLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Blocked</span></p></body></html>"))
+        self.addNewButton.setText(_translate("MainWindow", "Add New"))
+        self.blockedSitesLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Blocked Sites</span></p></body></html>"))
 
     def connectActions(self, MainWindow):
         self.saveButton.clicked.connect(self.clicked)
-        #self.addNewButton.clicked.connect(self.clicked)
+
+        # Add New Row
         self.addNewButton.clicked.connect(self.newRow)
 
 
@@ -130,10 +141,27 @@ class Ui_MainWindow(object):
 
     def newRow(self):
         print("adding new row...")
-        # Add functionality for adding a new row here!
-        #
-        #
+        # Text Field
+        newSite = QtWidgets.QTextEdit(self.scrollAreaWidgetContents)
+        newSite.setMinimumSize(QtCore.QSize(351, 31))
+        newSite.setMaximumSize(QtCore.QSize(351, 31))
+        newSite.setObjectName("newSite")
+        self.gridLayout.addWidget(newSite, self.row, 0, 1, 1)
 
+        # Checkbox
+        newCheckBox = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
+        newCheckBox.setText("")
+        newCheckBox.setObjectName("newCheckBox")
+        self.gridLayout.addWidget(newCheckBox, self.row, 1, 1, 1, QtCore.Qt.AlignHCenter)
+
+        # Delete
+        _translate = QtCore.QCoreApplication.translate
+        newDeleteButton = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        newDeleteButton.setObjectName("newDeleteButton")
+        newDeleteButton.setText(_translate("MainWindow", "Delete"))
+        self.gridLayout.addWidget(newDeleteButton, self.row, 2, 1, 1)
+
+        self.row += 1
 
 
 if __name__ == "__main__":
